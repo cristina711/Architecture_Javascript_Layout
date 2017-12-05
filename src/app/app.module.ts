d0c295ac52dd4bc1196b0d1e429da7b26e625c10
Qian Yang <qianyang@Qians-MacBook-Pro.local>
2017-12-04 18:03:39 -0800

    initial message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
